393845f1b1a8b9548a8e04aa81a7cc83d25f11a1
usma11dia0 <usma11dia0@gmail.com>
2017-08-03 16:53:04 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+Task.create(content: 'test', status: 'test_ttt')
+
+(1..10).each do |number|
+  Task.create(content: 'test content' + number.to_s ,status: 'test status' + number.to_s)
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
